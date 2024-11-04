924e6d541b6ce994748701330ac6361ba05492a6
matthewjaeyounghan <matthewhan1103@gmail.com>
2024-11-04 18:26:52 -0500

    Edits to main page

--- main.js
+++ main.js
@@ -9,7 +9,22 @@ const {logout, login} = require('./supabase');
 const { getStudyResources } = require('./supabase');
 
 app.get('/', (req, res) => {
-    res.send('Welcome to StudyMAD!')
+    res.send(`
+        <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>Welcome to StudyMAD</title>
+            </head>
+            <body>
+                <h1>Welcome to StudyMAD!</h1>
+                <p>Click the button below to go to the login page:</p>
+                <button onclick="window.location.href='/login'">Go to Login</button>
+            </body>
+            </html>
+        `)
 })
 
 // Login form
@@ -33,7 +48,6 @@ app.post('/login/post', async (req, res) => {
 app.get('/logout', async (req,res) => {
     try {
         await logout()
-
         const data = await getStudyResources()
         res.send(data)
     } catch (error) {
