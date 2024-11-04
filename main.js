ebd183edda9b2434955c323d768d591d53245cf7
matthewjaeyounghan <matthewhan1103@gmail.com>
2024-11-04 16:34:19 -0500

    Bulk

--- main.js
+++ main.js
@@ -2,5 +2,42 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const {logout, login} = require('./supabase');
+const { getStudyResources } = require('./supabase');
 
-console.log("test");
+app.get('/', (req, res) => {
+    res.send('Welcome to StudyMAD!')
+})
+
+// Login form
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.post('/login/post', async (req, res) => {
+    let username = req.body.username
+    let password = req.body.password
+    try {
+        await login(username, password)
+        const data = await getStudyResources()
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// Logout
+app.get('/logout', async (req,res) => {
+    try {
+        await logout()
+
+        const data = await getStudyResources()
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
